5b420fd57a83dde6bb790120756ccf0675a8be5c
Peter Freschi <pfreschi@uw.edu>
2017-03-31 10:44:52 -0700

    init

---
+++ FoodSavr/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FoodSavr
+//
+//  Created by Peter Freschi on 3/31/17.
+//  Copyright © 2017 FoodSavr. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
