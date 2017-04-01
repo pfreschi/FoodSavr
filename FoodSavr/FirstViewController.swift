2cd4ec82f30ac484d7e7f6f8332ebd4260fcbe1b
Xiaowen Feng <xfeng1@uw.edu>
2017-04-01 16:11:35 -0700

    Added firebase setting and camera feature

--- FoodSavr/FirstViewController.swift
+++ FoodSavr/FirstViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +18,32 @@ class FirstViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    var imagePicker: UIImagePickerController!
+ 
+    @IBAction func takePhoto(_ sender: UIButton) {
+        if (UIImagePickerController.isSourceTypeAvailable(.camera))  {
+        
+            imagePicker =  UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera
+        
+            self.present(imagePicker, animated: true, completion: nil)
+        } else {
+            print("the device does not have a camera")
+        }
+    }
+    @IBOutlet var imageView: UIImageView!
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = pickedImage
+            print("image taken: \(pickedImage)")
+        }
+        self.dismiss(animated: true, completion: nil)
+        //pickedImage.image  = info[UIImagePickerControllerOriginalImage] as! UIImage!;﻿
     }
 
 
