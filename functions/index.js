3d342a4093c4da03ffec3877095c4dc73553d11d
Sayna Parsi <parsis@uw.edu>
2017-04-29 12:19:38 -0700

    adding separate items

--- functions/index.js
+++ functions/index.js
@@ -6,58 +6,77 @@ admin.initializeApp(functions.config().firebase);
 var db = admin.database();
 var ref = db.ref();
 
+
+function getReceiptText(e) {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var response = JSON.parse(xhr.responseText);
+      return response.ParsedResults[0].ParsedText;
+
+    }
+}
+
 // creates function to get text from receipt and save items
 exports.getReceiptText1 = functions.database.ref('/receipts/{receiptId}/')
     .onWrite(event => {
-    	console.log("write event occured");
-
-        var itemsref = db.ref('/items/');
+      	console.log("write event occured");
+        const original = event.data.val();
+        var itemsref = db.ref('/items/').child(original.creatorId);
         var items = ["bananas", "apples", "broccoli", "milk", "flour"];
         var d = new Date();
         var updates = {};
 
-		for (index = 0; index < items.length; ++index) {
-			// generate itemId 
-			var newPostKey = itemsref.push().key;
-		    console.log(items[index]);
+    		for (index = 0; index < items.length; ++index) {
+    			// generate itemId
+    			var newPostKey = itemsref.push().key;
+    		    console.log(items[index]);
 
-			var postData = {
-			    creatorId: "Sayna",
-			    dateAdded: d.getFullYear(),
-			    deleted: false,
-			    disposed: false,
-			    expirationDate: "08, 25, 2017",
-			    name: items[index]
-			};
-			
-			updates[newPostKey] = postData;
-		}
-		console.log(updates);
-		return itemsref.update(updates);
+        API_KEY = "2c6009920c88957";
+        image_url = "https://firebasestorage.googleapis.com/v0/b/foodsavr-1347f.appspot.com/o/receipts%2Fg2rrAida7mUczgUb4Qwn9uCiVd32%2F513484257067.jpg?alt=media&token=7239ee08-ed37-4e20-af2c-95f33268ec2a";
+        request_url = "https://api.ocr.space/parse/imageurl?apikey=" + API_KEY + "&url=" + image_url;
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', request_url, true);
+        xhr.send();
+
+        
+  			var postData = {
+  			    creatorId: original.creatorId,
+  			    dateAdded: (d.getFullyear().toString() + " " + ("0" + d.getMonth()).slice(-2).toString() + " " + ("0" + d.getDate()).slice(-2).toString(),
+  			    deleted: false,
+  			    disposed: false,
+  			    expirationDate: "08, 25, 2017",
+  			    name: items[index]
+  			};
+
+    	  updates[newPostKey] = postData;
+    		}
+  		  console.log(updates);
+  		  // return itemsref.update(updates);
     });
 
 
+https://firebasestorage.googleapis.com/v0/b/foodsavr-1347f.appspot.com/o/receipts%2Fg2rrAida7mUczgUb4Qwn9uCiVd32%2F513831376827.jpg?alt=media&token=f651b07a-343d-4f75-860d-da131182f201
 
 
 
 // // create the request_url
-// API_KEY = APIKEY
-// image_url = "https://firebasestorage.googleapis.com/v0/b/foodsavr-1347f.appspot.com/o/receipts%2Fg2rrAida7mUczgUb4Qwn9uCiVd32%2F513484257067.jpg?alt=media&token=7239ee08-ed37-4e20-af2c-95f33268ec2a";
-// request_url = "https://api.ocr.space/parse/imageurl?apikey=" + API_KEY + "&url=" + image_url;
+API_KEY = "2c6009920c88957";
+image_url = "https://firebasestorage.googleapis.com/v0/b/foodsavr-1347f.appspot.com/o/receipts%2Fg2rrAida7mUczgUb4Qwn9uCiVd32%2F513484257067.jpg?alt=media&token=7239ee08-ed37-4e20-af2c-95f33268ec2a";
+request_url = "https://api.ocr.space/parse/imageurl?apikey=" + API_KEY + "&url=" + image_url;
 //
 // // sending request
-// var xhr = new XMLHttpRequest();
-// xhr.open('GET', request_url, true);
-// xhr.send();
+var xhr = new XMLHttpRequest();
+xhr.open('GET', request_url, true);
+xhr.send();
 //
 // // returns text from image
-// function getReceiptText(e) {
-//     if (xhr.readyState == 4 && xhr.status == 200) {
-//       var response = JSON.parse(xhr.responseText);
-//       return response.ParsedResults[0].ParsedText;
-//
-//     }
-// }
+function getReceiptText(e) {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var response = JSON.parse(xhr.responseText);
+      return response.ParsedResults[0].ParsedText;
+
+    }
+}
 //
 // // when the state changes calls the function to submit request
 // xhr.onreadystatechange = getReceiptText;
--- functions/test/testing.js
+++ functions/test/testing.js
@@ -5,4 +5,3 @@ const fakeEvent = {
 };
 
 myFunctions.makeUppercase(fakeEvent);
-
