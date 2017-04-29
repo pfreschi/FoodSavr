198684aa5c4921c139e75ed1c8cdc948d3ce9abd
Peter Freschi <pfreschi@uw.edu>
2017-04-29 11:24:05 -0700

    recipe progress

---
+++ FoodSavr/RecipeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RecipeViewController.swift
+//  FoodSavr
+//
+//  Created by Peter Freschi on 4/21/17.
+//  Copyright © 2017 FoodSavr. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import FirebaseDatabase
+import FirebaseAuth
+import Alamofire
+
+
+class RecipeViewController: UIViewController {
+    
+    var fakeIngredientList = ["apple","eggs","rice","turkey","mustard","lettuce"]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // gather info from cloud functions result
+        
+        
+    }
+    
+    
+}
+
+
